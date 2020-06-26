695b9c3e8fc7901a75730a526f7cfaa406f16f86
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-06-26 01:45:25 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -10,7 +10,7 @@ def fourth_challenge
           :attitude => "worried"
         },
         {
-          :name => "Mercutio",
+          :name => "Mercutios",
           :age => "18",
           :attitude => "hot-headed"
         }
